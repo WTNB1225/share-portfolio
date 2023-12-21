8e4725598d735468f7d7da5193298b66d8d6d0cc
WTNB1225 <w.swallowsfan@gmail.com>
2023-12-21 10:43:53 +0900

    sign up

--- back/app/controllers/users_controller.rb
+++ back/app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json:@user, status: :created
+    else
+      render json:@user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- back/config/routes.rb
+++ back/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  post "/login",   to: 'session#create'
   resources :users
 end
