65c4eb481223fa05d6322adb29ff391a95bfdc0c
WTNB1225 <144193938+WTNB1225@users.noreply.github.com>
2023-12-23 11:38:22 +0900

    secure auth

--- back/app/controllers/session_controller.rb
+++ back/app/controllers/session_controller.rb
@@ -8,7 +8,7 @@ class SessionController < ApplicationController
     @user = User.find_by(email:params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
       reset_session
-      remember @user
+      params[:session][:remember_me] == "1" ? remember(@user) : forget(@user)
       log_in @user
       render json: @user, status: :created
     else
