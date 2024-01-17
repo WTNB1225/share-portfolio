7e38ee93a4b36c49c0c7dfdf3b90c9c1cfab668f
WTNB1225 <w.swallowsfan@gmail.com>
2024-01-17 11:41:12 +0900

    基本的なbookmark

---
+++ back/app/controllers/bookmarks_controller.rb
@@ -0,0 +1,48 @@
+class BookmarksController < ApplicationController
+  def index
+    @bookmark = Bookmark.all
+    render json: @bookmark
+  end
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      render json:@bookmark, status: :created
+    else
+      render json:@bookmark.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find_by(user_id: params["user_id"], post_id: params["post_id"])
+    if @bookmark
+      @bookmark.destroy
+      render json: @bookmark
+    else
+      render json: @bookmark.errors, status: :unprocessable_entity
+    end
+  end
+
+  def count
+    @bookmark = Bookmark.where(post_id: params[:id])
+    render json: @bookmark.count
+  end
+
+  def bookmark?
+    @bookmark = Bookmark.find_by(user_id: params["user_id"], post_id: params[:post_id])
+    if @bookmark
+      render json: true
+    else
+      render json: false
+    end
+  end
+
+  def users_bookmark
+    @bookmark = Bookmark.where(user_id: params[:id])
+    render json:@bookmark
+  end
+
+  private
+    def bookmark_params
+      params.require(:bookmark).permit(:user_id, :post_id)
+    end
+end
--- front/src/app/[user]/bookmark/page.tsx
+++ front/src/app/[user]/bookmark/page.tsx
@@ -1,13 +1,15 @@
-"use client"
+"use client";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import Header from "@/components/Header";
 import style from "./page.module.css";
 import { useRouter, usePathname } from "next/navigation";
 import { useCheckLoginStatus } from "@/hook/useCheckLoginStatus";
 import { useGetCsrfToken } from "@/hook/useGetCsrfToken";
+import { get } from "http";
 
 type Data = {
+  name:string;
   id: string;
   title: string;
   content: string;
@@ -19,6 +21,59 @@ type Data = {
 };
 
 export default function Bookmark() {
+  const [name, setName] = useState("");
+  const [loading, setLoading] = useState<boolean>(true);
+  const [data, setData] = useState<Data>({name:"", id:"", title:"", content:"", image:"", images_url:"", username:"", avatar_url:"", post_id:""});
 
+  const pathname = usePathname();
+  const username = pathname.split("/").reverse()[1];
+
+  useEffect(() => {
+    async function checkLoginStatus() {
+      try {
+        const response = await axios.get("http://localhost:3000/logged_in_user", {
+          withCredentials: true,
+        });
+        setData(response.data);
+        getBookmark(response.data.id); 
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    }
+  
+    async function getBookmark(id: string) { 
+      try{
+        const response = await axios.get(`http://localhost:3000/${id}/bookmarks`);
+        console.log(response.data);
+      } catch(e) {
+        console.log(e);
+      }
+    }
+  
+    checkLoginStatus();
+  }, []); 
+
+  if(loading == false) {
+    if(data.name !== username){
+      return (
+        <div>
+          <Header />
+          <h1>ブックマーク</h1>
+          <h1>あなたはこのページを見ることはできません</h1>
+        </div>
+      )
+    }
+  }
+  
+  return (
+    loading == false &&(
+    <div>
+      <Header />
+      <h1>ブックマーク</h1>
+
+    </div>
+    )
+  );
+  
 }
-
