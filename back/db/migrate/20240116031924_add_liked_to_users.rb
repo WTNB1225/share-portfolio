dd6c516533f688810e6f1e24d19d21e00f73f194
WTNB1225 <144193938+WTNB1225@users.noreply.github.com>
2024-01-28 22:13:12 +0900

    fix

--- back/db/migrate/20240116031924_add_liked_to_users.rb
+++ back/db/migrate/20240116031924_add_liked_to_users.rb
@@ -1,6 +1,5 @@
 class AddLikedToUsers < ActiveRecord::Migration[7.1]
   def change
-    remove_column :users, :liked
     add_column :users, :liked, :string, default: [].to_s
     add_index :users, :liked
   end
