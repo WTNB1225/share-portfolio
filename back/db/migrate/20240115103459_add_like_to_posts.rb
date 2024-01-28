067ac3e337cb6a8fd2949a3ece02c4e627493d4f
WTNB1225 <144193938+WTNB1225@users.noreply.github.com>
2024-01-28 22:11:32 +0900

    fix

--- back/db/migrate/20240115103459_add_like_to_posts.rb
+++ back/db/migrate/20240115103459_add_like_to_posts.rb
@@ -1,6 +1,5 @@
 class AddLikeToPosts < ActiveRecord::Migration[7.1]
   def change
-    remove_column :posts, :like, :integer
     add_column :posts, :like, :string
     add_index :posts, :like
   end
