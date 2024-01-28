8c0df441416bd736decd1db1c81cd6f70b328053
WTNB1225 <w.swallowsfan@gmail.com>
2024-01-29 00:07:18 +0900

    fix

--- front/next.config.js
+++ front/next.config.js
@@ -2,7 +2,7 @@
 const path = require("path")
 const nextConfig = {
   images:{
-    domains:["localhost", "share-portfolio-api.onrender.com","share-portfolio.vercel.app"]
+    domains:[ "share-portfolio-api.onrender.com"]
   },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
