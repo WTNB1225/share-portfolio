82f9acafd4e9f841974e19f060c19762aed9e5c7
WTNB1225 <144193938+WTNB1225@users.noreply.github.com>
2024-01-28 20:45:24 +0900

    fix

--- front/next.config.js
+++ front/next.config.js
@@ -2,7 +2,7 @@
 const path = require("path")
 const nextConfig = {
   images:{
-    domains:["localhost", "https://share-portfolio-git-product-wtnbjps-projects.vercel.app"]
+    domains:["localhost", "share-portfolio-git-product-wtnbjps-projects.vercel.app"]
   },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
