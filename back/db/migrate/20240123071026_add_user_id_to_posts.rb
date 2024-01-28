b0c5ed41cc1ec719f1abc679900a4824bc4f0b90
WTNB1225 <144193938+WTNB1225@users.noreply.github.com>
2024-01-28 22:14:46 +0900

    fix

--- back/db/migrate/20240123071026_add_user_id_to_posts.rb
+++ back/db/migrate/20240123071026_add_user_id_to_posts.rb
@@ -1,7 +1,5 @@
 class AddUserIdToPosts < ActiveRecord::Migration[7.1]
   def change
-    remove_column :posts, :userid, :string
-    remove_column :posts, :user_id, :integer
     add_column :posts, :userid, :string
     add_column :posts, :user_id, :integer
   end
