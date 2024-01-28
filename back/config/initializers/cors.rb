e25f80f9c966aedb854006c74a9d8d604e297b83
WTNB1225 <144193938+WTNB1225@users.noreply.github.com>
2024-01-28 22:55:46 +0900

    fix

--- back/config/initializers/cors.rb
+++ back/config/initializers/cors.rb
@@ -7,9 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins "https://share-portfolio-git-product-wtnbjps-projects.vercel.app" 
     resource "*",
-      credentials: false,
+      credentials: true,
       headers: :any,
       expose: ['X-CSRF-Token'],
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
