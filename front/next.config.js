cd168a4d48734ce293f6bbb1e93c7b81fb3d3fcf
WTNB1225 <144193938+WTNB1225@users.noreply.github.com>
2024-01-28 21:55:46 +0900

    chagne endpoint

--- front/next.config.js
+++ front/next.config.js
@@ -2,7 +2,7 @@
 const path = require("path")
 const nextConfig = {
   images:{
-    domains:["localhost", ]
+    domains:["localhost", "share-portfolio-api.onrender.com"]
   },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
