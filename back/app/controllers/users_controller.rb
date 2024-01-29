11723ed5bfbbb71158ac1767d8a34e4d8cf307b2
WTNB1225 <w.swallowsfan@gmail.com>
2024-01-29 15:35:02 +0900

    render change

--- back/app/controllers/users_controller.rb
+++ back/app/controllers/users_controller.rb
@@ -5,23 +5,18 @@ class UsersController < ApplicationController
   protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token, only: [:create]
 
-  def index
-    @users = User.all
-    render json: @users
-  end
-
   def show
     @user = User.find_by(name:params[:id])
     if @user
-      user = @user.as_json(include: :avatar).merge(avatar_url: url_for(@user.avatar))
+      user = @user.as_json(only: [:name, :id]).merge(avatar_url: url_for(@user.avatar))
+      render json: user
     end
-    render json: user
   end
 
   def show_by_id
     @user = User.find_by(id:params[:user_id])
     if @user
-      user = @user.as_json(include: :avatar).merge(avatar_url: url_for(@user.avatar))
+      user = @user.as_json(only: [:name, :id]).merge(avatar_url: url_for(@user.avatar))
       render json: user
     else
       render json: @user.errors, status: :unprocessable_entity
