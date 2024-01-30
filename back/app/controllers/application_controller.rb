e4a514005308d22d1372c71d67bb6777325765fd
WTNB1225 <w.swallowsfan@gmail.com>
2024-01-30 16:39:28 +0900

    fix

--- back/app/controllers/application_controller.rb
+++ back/app/controllers/application_controller.rb
@@ -16,8 +16,7 @@ class ApplicationController < ActionController::API
 
   #apiの認証 外部からのアクセスを制限する
   def authenticate_request
-    auth_header = request.headers['Authorization']
-    auth_token = auth_header&.gsub('Bearer ', '')
+    auth_token = request.headers['Authorization']
 
     if auth_token
       begin
