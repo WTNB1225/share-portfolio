01ee0f08eb52c2a6a0f0cc745a45d045c52fc8b5
WTNB1225 <w.swallowsfan@gmail.com>
2024-01-31 12:17:03 +0900

    change cors

--- back/db/migrate/20240131004615_remove_avatar_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAvatarFromUsers < ActiveRecord::Migration[7.1]
-  def change
-    remove_column :users, :avatar, :string
-  end
-end
--- front/next.config.js
+++ front/next.config.js
@@ -2,7 +2,7 @@
 const path = require("path")
 const nextConfig = {
   images:{
-    domains:["share-portfolio-api.onrender.com", "share-portfolio.vercel.app"]
+    domains:["share-portfolio-api.onrender.com", "share-portfolio-wut1.vercel.app"]
   },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
