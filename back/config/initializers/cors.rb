e31d1f93a73951428abc90d7a7ec039c6aae7584
WTNB1225 <w.swallowsfan@gmail.com>
2024-01-31 23:18:42 +0900

    fix cors

--- back/config/initializers/cors.rb
+++ back/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins  "http://localhost:8080" #https://share-portfolio-wut1.vercel.app"
+    origins "https://share-portfolio-wut1.vercel.app"
     resource "*",
       credentials: true,
       headers: :any,
