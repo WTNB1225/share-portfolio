355dba3c425dcd60bee105d875a97cb2396cf499
WTNB1225 <144193938+WTNB1225@users.noreply.github.com>
2024-01-31 12:40:39 +0900

    fix

--- back/db/seeds.rb
+++ back/db/seeds.rb
@@ -1,8 +1,7 @@
-user = User.create!(
+user = User.create(
   name: "admin",
   email:"admin@email.com",
   password: "password",
   password_confirmation: "password",
-  admin: true
-)
+  admin: true)
 user.avatar.attach(io: File.open("https://pub-a05d828609984db8b2239cd099a20aac.r2.dev/282639_bird_xl-1024-v1-0.png"))
