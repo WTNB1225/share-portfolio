da4faed1812399a1be590d619398c44432092957
WTNB1225 <w.swallowsfan@gmail.com>
2024-01-31 14:29:14 +0900

    seed

--- back/db/seeds.rb
+++ back/db/seeds.rb
@@ -1,5 +1,5 @@
 require 'open-uri'
-user = User.create(
+user = User.new(
   name: "admin",
   email:"admin@email.com",
   password: "password",
@@ -7,3 +7,4 @@ user = User.create(
   admin: true)
 file = URI.open("https://pub-a05d828609984db8b2239cd099a20aac.r2.dev/282639_bird_xl-1024-v1-0.png")
 user.avatar.attach(io: file, filename: 'avatar.png')
+user.save!
