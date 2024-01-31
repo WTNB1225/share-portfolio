098f6ba8af3d8b16daf80e35b0407eddb9c9eda6
WTNB1225 <144193938+WTNB1225@users.noreply.github.com>
2024-01-31 23:18:42 +0900

    fix cors

--- front/next.config.js
+++ front/next.config.js
@@ -2,7 +2,7 @@
 const path = require("path")
 const nextConfig = {
   images:{
-    domains:["share-portfolio-api.onrender.com", "share-portfolio-wut1.vercel.app", "localhost"]
+    domains:["share-portfolio-api.onrender.com", "share-portfolio-wut1.vercel.app"]
   },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
