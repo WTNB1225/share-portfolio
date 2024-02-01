a0d7ffa1031e1e89d184a1b85f55299af7a1e362
WTNB1225 <w.swallowsfan@gmail.com>
2024-02-01 14:09:48 +0900

    fix token

--- front/src/components/Comment.tsx
+++ front/src/components/Comment.tsx
@@ -7,6 +7,9 @@ import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
 import Cookies from "js-cookie";
 import Markdown from "./Markdown";
+import { useState, useEffect } from "react";
+import { headers } from "next/headers";
+import { useGetCsrfToken } from "@/hook/useGetCsrfToken";
 
 export default function Comment({
   id,
@@ -29,12 +32,24 @@ export default function Comment({
 }) {
   const theme = Cookies.get("theme") || "#F8F9FA";
 
+  const [token, setToken] = useState<string>("");
+  const csrfToken = useGetCsrfToken();
+
+  useEffect(() => {
+    setToken(csrfToken);
+  }, [csrfToken]);
+
   //コメントを削除する関数 admin or 投稿者のみ
   const handleDelete = async () => {
     if (postAuthor === currentUser || admin === 1) {
       try {
         const response = await axios.delete(
-          `${process.env.NEXT_PUBLIC_ENDPOINT}/comments/${id}`
+          `${process.env.NEXT_PUBLIC_ENDPOINT}/comments/${id}`,
+          {
+            headers: {
+              "X-CSRF-TOKEN": token,
+            },
+          }
         );
         if (response.status == 200) {
           onDelete(id);
