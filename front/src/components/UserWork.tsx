dfc8492ce91cf2d485eb8611fc2856c4e67d42d5
WTNB1225 <w.swallowsfan@gmail.com>
2024-02-11 00:24:02 +0900

    fix

--- front/src/components/UserWork.tsx
+++ front/src/components/UserWork.tsx
@@ -216,7 +216,7 @@ export default function UserWork({
           <div style={{ position: "relative", height: "250px" }}>
             <Image
               alt="image"
-              src={encodeURIComponent(image)}
+              src={image}
               height={250}
               width={250}
               objectFit="cover"
